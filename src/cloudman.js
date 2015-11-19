fdff135802c7f587fc71cb3bd49db0f44ef09011
Willy Ovalle <willyovalle16@gmail.com>
2015-11-18 22:46:41 -0400

    Enhancement: adding status method in cloudman.js, closes #20

--- src/cloudman.js
+++ src/cloudman.js
@@ -1,15 +1,6 @@
 /*
 	cloudman::
 		description: here we should have a provider-less implementation for the API.
-
-
-	cloudman::status  
-		description: 	get status vm's (machines, instances) in provided keyNames (in the account 
-						related to the keyName, see cred.js).
-	
-		input: Array of unique keyName  ['keyname1', 'keyname2'...] (see keyName in cred.js).
-
-		output: Array of instances. [{}, {}...] (See src/models/instances).
 	
 
 	cloudman::stop 
@@ -48,3 +39,88 @@
 		- Everything will return promises. When pass = resolve output params. When fail = reject err.
 
 */
+var 	_ = require('lodash'),
+ 		Promise = require('bluebird'),
+	 	credentials = require('./cred'),
+	 	aws = require('./providers/aws/aws');
+
+
+/*
+* cloudman::status
+*
+* description: 		get status vm's (machines, instances) in provided keyNames (in the account
+* 					related to the keyName, see cred.js).
+*
+* input: 	Array of unique keyName  ['keyname1', 'keyname2'...] (see keyName in cred.js).
+*
+* output: 	Array of instances. [{}, {}...] (See src/models/instances).
+*
+* */
+exports.status = function(keyNames){
+	return new Promise(function(resolve, reject){
+		var cred = setCredentials(keyNames, credentials);
+		var awsCred = filterProvider(cred, 'aws');
+
+		var promise_aws = _.map(awsCred, function(cr){
+			return aws.status(cr);
+		});
+
+		return Promise.all(promise_aws)
+			.then(flattenize)
+			.then(resolve);
+	});
+
+};
+
+/*
+ * cloudman::status
+ *
+ * description: 	Helper to flatten arrays.
+ *
+ * input: 	data to flattenize.
+ *
+ * output: 	flattenized array.
+ *
+ * */
+var flattenize = function(data){
+	return new Promise(function(resolve, reject){
+		resolve(_.flatten(data));
+	});
+};
+
+/*
+ * cloudman::setCredentials
+ *
+ * description: 	Receives one array of keyNames and one array of credentials
+  * 				and returns all the credentials with the given keyNames.
+ *
+ * input: 	keyNames array
+ * 			credentials array.
+ *
+ * output: 	filtered array.
+ *
+ * */
+var setCredentials = function(_keyNames, _credentials){
+	return _.filter(_credentials, function(cred){
+		return _keyNames.indexOf(cred.keyName)> -1;
+	});
+};
+
+/*
+ * cloudman::filterProvider
+ *
+ * description: 	Receives one array of credentials and one provider and
+ * 					returns all the credentials for the given provider.
+ *
+ * input: 	keyNames array
+ * 			credentials array.
+ *
+ * output: 	filtered array.
+ *
+ * */
+var filterProvider = function(_cred, _provider){
+	return _.filter(_cred, function(cr){
+		return cr.provider === _provider;
+	});
+};
+
